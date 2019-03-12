1b914b3df83ba01361bf0d36daef674d77b7e29e
i-r-b <iannrb@gmail.com>
2019-03-12 15:40:10 -0500

    js for reqdet

--- static/js/request_page.js
+++ static/js/request_page.js
@@ -1,11 +1,21 @@
 function swapStatus(){
     var statusline = document.getElementById('statusline');
-    var startbutton = document.getElementById('startbutton')
+    var startbutton = document.getElementById('startbutton');
+    var hiddenRows = document.getElementsByClassName('selectreagent');
+    var numhidrow = hiddenRows.length;
+    console.log(hiddenRows)
     if (statusline.innerHTML == 'Status: Queued') {
       statusline.innerHTML = 'Status: In Progress';
       startbutton.innerHTML = 'Re-queue';
     } else if (statusline.innerHTML == 'Status: In Progress') {
       statusline.innerHTML = 'Status: Queued';
       startbutton.innerHTML = 'Start Recipe';
+    }
+    for(var i=0; i<numhidrow; i++){
+      if(statusline.innerHTML == 'Status: Queued'){
+        hiddenRows[i].setAttribute('hidden','true');
+      } else if(statusline.innerHTML == 'Status: In Progress'){
+        hiddenRows[i].removeAttribute('hidden');
+      }
     }
 }
